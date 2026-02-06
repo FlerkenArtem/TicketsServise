f509eedfd6b15eaa633898f913299782deb044c3
FlerkenArtem <vokak738@yandex.ru>
2026-02-06 13:03:28 +0300

    Реализован класс подключения к БД

---
+++ TicketsServise/DatabaseHelper.cs
@@ -0,0 +1,98 @@
+﻿using Npgsql;
+using System.Data;
+
+namespace TicketsServise
+{
+    public static class DatabaseHelper
+    {
+        private static string connectionString = "Host=localhost;Database=tickets_servise;Username=postgres;Password=postgres";
+        public static string ConnectionString => connectionString;
+        public static bool TestConnection()
+        {
+            try
+            {
+                using (var connection = new NpgsqlConnection(connectionString))
+                {
+                    connection.Open();
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка подключения к БД: " + ex.Message);
+                return false;
+            }
+        }
+        public static DataTable ExecuteQuery(string query, NpgsqlParameter[] parameters = null)
+        {
+            var dataTable = new DataTable();
+
+            try
+            {
+                using (var connection = new NpgsqlConnection(connectionString))
+                using (var command = new NpgsqlCommand(query, connection))
+                {
+                    if (parameters != null)
+                    {
+                        command.Parameters.AddRange(parameters);
+                    }
+
+                    connection.Open();
+                    using (var adapter = new NpgsqlDataAdapter(command))
+                    {
+                        adapter.Fill(dataTable);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка выполнения запроса: " + ex.Message);
+            }
+            return dataTable;
+        }
+        public static object ExecuteScalar(string query, NpgsqlParameter[] parameters = null)
+        {
+            try
+            {
+                using (var connection = new NpgsqlConnection(connectionString))
+                using (var command = new NpgsqlCommand(query, connection))
+                {
+                    if (parameters != null)
+                    {
+                        command.Parameters.AddRange(parameters);
+                    }
+
+                    connection.Open();
+                    return command.ExecuteScalar;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка выполнения запроса: " + ex.Message);
+                return null;
+            }
+        }
+        public static int ExecuteNonQuery(string query, NpgsqlParameter[] parameters = null)
+        {
+            try
+            {
+                using (var connecation = new NpgsqlConnection(connectionString))
+                using (var command = new NpgsqlCommand(query, connecation))
+                {
+                    if (parameters != null)
+                    {
+                        command.Parameters.AddRange(parameters);
+                    }
+
+                    connecation.Open();
+                    return command.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка выполнения запроса: " + ex.Message);
+                return -1;
+            }
+        }
+    }
+}
