9e7e8d4100f15e37da78da9076281d746423a583
FlerkenArtem <vokak738@yandex.ru>
2026-02-12 16:32:18 +0300

    Реализована форма для демонстрации интерфейсов

--- TicketsServise/Interfaces/InterfacesDemoForm.Designer.cs
+++ TicketsServise/Interfaces/InterfacesDemoForm.Designer.cs
@@ -28,12 +28,121 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "InterfacesDemoForm";
+            btnCreate = new Button();
+            btnSortComparable = new Button();
+            btnSortComparer = new Button();
+            btnForeach = new Button();
+            btnClone = new Button();
+            btnDispose = new Button();
+            listBoxCollections = new ListBox();
+            listBoxLog = new ListBox();
+            SuspendLayout();
+            // 
+            // btnCreate
+            // 
+            btnCreate.Location = new Point(12, 12);
+            btnCreate.Name = "btnCreate";
+            btnCreate.Size = new Size(94, 29);
+            btnCreate.TabIndex = 0;
+            btnCreate.Text = "Создать";
+            btnCreate.UseVisualStyleBackColor = true;
+            btnCreate.Click += btnCreate_Click;
+            // 
+            // btnSortComparable
+            // 
+            btnSortComparable.Location = new Point(112, 12);
+            btnSortComparable.Name = "btnSortComparable";
+            btnSortComparable.Size = new Size(205, 29);
+            btnSortComparable.TabIndex = 1;
+            btnSortComparable.Text = "Сортировка IComparable";
+            btnSortComparable.UseVisualStyleBackColor = true;
+            btnSortComparable.Click += btnSortComparable_Click;
+            // 
+            // btnSortComparer
+            // 
+            btnSortComparer.Location = new Point(323, 12);
+            btnSortComparer.Name = "btnSortComparer";
+            btnSortComparer.Size = new Size(179, 29);
+            btnSortComparer.TabIndex = 2;
+            btnSortComparer.Text = "Сортировка IComparer";
+            btnSortComparer.UseVisualStyleBackColor = true;
+            btnSortComparer.Click += btnSortComparer_Click;
+            // 
+            // btnForeach
+            // 
+            btnForeach.Location = new Point(12, 47);
+            btnForeach.Name = "btnForeach";
+            btnForeach.Size = new Size(139, 29);
+            btnForeach.TabIndex = 3;
+            btnForeach.Text = "Перебор Foreach";
+            btnForeach.UseVisualStyleBackColor = true;
+            btnForeach.Click += btnForeach_Click;
+            // 
+            // btnClone
+            // 
+            btnClone.Location = new Point(157, 47);
+            btnClone.Name = "btnClone";
+            btnClone.Size = new Size(130, 29);
+            btnClone.TabIndex = 4;
+            btnClone.Text = "Клонирование";
+            btnClone.UseVisualStyleBackColor = true;
+            btnClone.Click += btnClone_Click;
+            // 
+            // btnDispose
+            // 
+            btnDispose.Location = new Point(293, 47);
+            btnDispose.Name = "btnDispose";
+            btnDispose.Size = new Size(209, 29);
+            btnDispose.TabIndex = 5;
+            btnDispose.Text = "Освобождение ресурсов";
+            btnDispose.UseVisualStyleBackColor = true;
+            btnDispose.Click += btnDispose_Click;
+            // 
+            // listBoxCollections
+            // 
+            listBoxCollections.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            listBoxCollections.FormattingEnabled = true;
+            listBoxCollections.Location = new Point(12, 82);
+            listBoxCollections.Name = "listBoxCollections";
+            listBoxCollections.Size = new Size(758, 224);
+            listBoxCollections.TabIndex = 6;
+            // 
+            // listBoxLog
+            // 
+            listBoxLog.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            listBoxLog.FormattingEnabled = true;
+            listBoxLog.Location = new Point(12, 312);
+            listBoxLog.Name = "listBoxLog";
+            listBoxLog.Size = new Size(758, 224);
+            listBoxLog.TabIndex = 7;
+            // 
+            // InterfacesDemoForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(782, 553);
+            Controls.Add(listBoxLog);
+            Controls.Add(listBoxCollections);
+            Controls.Add(btnDispose);
+            Controls.Add(btnClone);
+            Controls.Add(btnForeach);
+            Controls.Add(btnSortComparer);
+            Controls.Add(btnSortComparable);
+            Controls.Add(btnCreate);
+            Name = "InterfacesDemoForm";
+            Text = "InterfacesDemoForm";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button btnCreate;
+        private Button btnSortComparable;
+        private Button btnSortComparer;
+        private Button btnForeach;
+        private Button btnClone;
+        private Button btnDispose;
+        private ListBox listBoxCollections;
+        private ListBox listBoxLog;
     }
 }
--- TicketsServise/Interfaces/InterfacesDemoForm.cs
+++ TicketsServise/Interfaces/InterfacesDemoForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics.Eventing.Reader;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
@@ -10,9 +11,100 @@ namespace TicketsServise.Interfaces
 {
     public partial class InterfacesDemoForm : Form
     {
+        private List<TicketCollection> _collections = new List<TicketCollection>();
+        private BindingSource _collectionsSrc =new BindingSource();
+
         public InterfacesDemoForm()
         {
             InitializeComponent();
+            _collectionsSrc.DataSource = _collections;
+            listBoxCollections.DataSource = _collectionsSrc;
+            listBoxCollections.DisplayMember = "ToString";
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e) // создание коллекции
+        {
+            _collections.Clear();
+
+            var coll1 = new TicketCollection { CustomerName = "Иванов", OrderDate = DateTime.Today };
+            coll1.AddTicket(new Ticket(1, "Концерт", DateTime.Now.AddDays(10), 1500));
+            coll1.AddTicket(new Ticket(2, "Концерт", DateTime.Now.AddDays(10), 1500));
+
+            var coll2 = new TicketCollection { CustomerName = "Петров", OrderDate = DateTime.Today };
+            coll2.AddTicket(new Ticket(3, "Матч", DateTime.Now.AddDays(20), 500));
+            coll2.AddTicket(new Ticket(4, "Матч", DateTime.Now.AddDays(20), 500));
+            coll2.AddTicket(new Ticket(5, "Матч", DateTime.Now.AddDays(20), 500));
+
+            var coll3 = new TicketCollection { CustomerName = "Сидоров", OrderDate = DateTime.Today };
+            coll3.AddTicket(new Ticket(6, "Спектакль", DateTime.Now.AddDays(30), 2500));
+            coll3.AddTicket(new Ticket(7, "Спектакль", DateTime.Now.AddDays(30), 2500));
+
+            _collections.Add(coll1);
+            _collections.Add(coll2);
+            _collections.Add(coll3);
+
+            _collectionsSrc.ResetBindings(false);
+            Log("Коллекции созданы");
+        }
+        private void btnSortComparable_Click(object sender, EventArgs e)
+        {
+            _collections.Sort();
+            _collectionsSrc.ResetBindings(false);
+            Log("Коллекции отсортированы по общей стоимости");
+        }
+        private void btnSortComparer_Click(object sender, EventArgs e)
+        {
+            _collections.Sort(new TicketCollectionComparer());
+            _collectionsSrc.ResetBindings(false);
+            Log("Коллекции отсортированы по количеству билетов");
+        }
+        private void btnForeach_Click(object sender, EventArgs e)
+        {
+            if (listBoxCollections.SelectedItem is TicketCollection selected)
+            {
+                Log($"Перебор билетов заказа {selected.CustomerName}: ");
+                foreach (var ticket in selected)
+                {
+                    Log($"  {ticket}");
+                }
+            }
+            else
+            {
+                Log("Выберите коллекцию");
+            }
+        }
+        private void btnClone_Click(object sender, EventArgs e)
+        {
+            if (listBoxCollections.SelectedItem is TicketCollection original)
+            {
+                var clone = (TicketCollection)original.Clone();
+                clone.CustomerName += "(Копия)";
+                _collections.Add(clone);
+                _collectionsSrc.ResetBindings(false);
+                Log("Создана копия коллекции");
+            }
+            else
+            {
+                Log("Выберите коллекцию для клонирования");
+            }
+        }
+        private void btnDispose_Click(object sender, EventArgs e)
+        {
+            if (listBoxCollections.SelectedItem is TicketCollection selected)
+            {
+                selected.Dispose();
+                Log($"Ресурсы коллекции {selected.CustomerName} освобождены");
+            }
+            else
+            {
+                Log("Выберите коллекцию");
+            }
+        }
+
+        private void Log(string message) // Вывод сообщений в ListBox
+        {
+            listBoxLog.Items.Add($"{DateTime.Now:T}: {message}");
+            listBoxLog.SelectedIndex = listBoxLog.Items.Count - 1;
         }
     }
 }
