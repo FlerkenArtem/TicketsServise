90f76f0ef03dadccacb45351c0a92e2d85cab15d
FlerkenArtem <vokak738@yandex.ru>
2026-02-20 14:37:43 +0300

    Изменена точка входа в программу

--- TicketsServise/Program.cs
+++ TicketsServise/Program.cs
@@ -1,4 +1,4 @@
-/*namespace TicketsServise
+namespace TicketsServise
 {
     internal static class Program
     {
@@ -14,38 +14,41 @@
             Application.Run(new Form1());
         }
     }
-}*/
-/*namespace TicketsServise.Interfaces // Запуск тестовой формы для демонстрации интерфейсов
-{
-    internal static class Program
-    {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new InterfacesDemoForm());
-        }
-    }
-}*/
-namespace TicketsServise.Struct // Запуск тестовой формы для демонстрации различий структуры и класса
-{
-    internal static class Program
-    {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new StructDemoForm());
-        }
-    }
 }
+
+//Альтернативные точки входа в программу для демонстрации 
+
+//namespace TicketsServise.Interfaces // Запуск тестовой формы для демонстрации интерфейсов
+//{
+//    internal static class Program
+//    {
+//        /// <summary>
+//        ///  The main entry point for the application.
+//        /// </summary>
+//        [STAThread]
+//        static void Main()
+//        {
+//            // To customize application configuration such as set high DPI settings or default font,
+//            // see https://aka.ms/applicationconfiguration.
+//            ApplicationConfiguration.Initialize();
+//            Application.Run(new InterfacesDemoForm());
+//        }
+//    }
+//}
+//namespace TicketsServise.Struct // Запуск тестовой формы для демонстрации различий структуры и класса
+//{
+//    internal static class Program
+//    {
+//        /// <summary>
+//        ///  The main entry point for the application.
+//        /// </summary>
+//        [STAThread]
+//        static void Main()
+//        {
+//            // To customize application configuration such as set high DPI settings or default font,
+//            // see https://aka.ms/applicationconfiguration.
+//            ApplicationConfiguration.Initialize();
+//            Application.Run(new StructDemoForm());
+//        }
+//    }
+//}
